2ab642351ac6bf069a4426f3032f8567e6f70fee
bleish-git <bleish@gmail.com>
2023-12-07 08:00:40 +0100

    update contable

--- agricontable/urls.py
+++ agricontable/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('anag/', include('anag_utenti.urls')),
     path('info/', include('info.urls')),
+    path('contable/', include('contable.urls')),
     path('', include('anag_utenti.urls')),
 ]
 
--- contable/admin.py
+++ contable/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ContoCOGE, PncGen
 
-# Register your models here.
+admin.site.register(ContoCOGE)
+admin.site.register(PncGen)
