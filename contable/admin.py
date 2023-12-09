e6aac3ef6c96214f50e28aeaa468579e036c6072
bleish-git <bleish@gmail.com>
2023-12-09 13:35:40 +0100

    update contable

--- contable/admin.py
+++ contable/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
-from .models import ContoCOGE, PncGen
+from .models import ContoCOGE,TipiDocCoge, PncGen, PncRighe
 #from mptt.admin import MPTTModelAdmin
 from django_mptt_admin.admin import DjangoMpttAdmin
 
-admin.site.register(ContoCOGE, DjangoMpttAdmin)
-admin.site.register(PncGen)
+
 
 #MPTT_ADMIN_LEVEL_INDENT = 20
 
+class PncRigheInline(admin.TabularInline):
+    model = PncRighe
+    class Meta:
+        verbose_name_plural = "Corpo del documento"
+
+class PncAdmin(admin.ModelAdmin):
+    ordering = ['numPnc']
+    list_display = ('numPnc','dataPnc')
+    inlines = (PncRigheInline,)
+    list_filter = ('esercizioCompetenza',)
+    search_fields = ['numPnc','tipoDocCoge']
+
+
+
+admin.site.register(ContoCOGE, DjangoMpttAdmin)
+admin.site.register(TipiDocCoge)
+admin.site.register(PncGen,PncAdmin)
--- contable/models.py
+++ contable/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-from datetime import date
+from datetime import date, datetime
 
 class ContoCOGE (MPTTModel):    
-    idContoCOGE = models.BigAutoField(primary_key=True)
-    nome = models.CharField(max_length=2)
-    descrizione = models.CharField("Descrizione",max_length=20)
+    nome = models.CharField(max_length=2,default='')
+    descrizione = models.CharField("Descrizione",max_length=50,default='')
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     class MPTTMeta:
@@ -17,44 +16,54 @@ class ContoCOGE (MPTTModel):
 
     def __str__(self):
         return str(self.nome) + " - " + self.descrizione
-
-class PncRighe (models.Model):
-    idPnc = models.BigAutoField(primary_key=True, default=0)
-    stato = models.CharField('Stato', max_length=30,default='',blank=True)
-    datacreazione = models.DateField('Data di inserimento',default=date.today)
-    #timestamp e variazioni con indicazione utentee
+    
 
 class TipiDocCoge (models.Model):
     codDoc = models.CharField('Codice Documento', max_length=4,default='',blank=False)
     DesDoc = models.CharField('Descrizione',max_length=30)
     #inserire puntatore a lista di opzioni collegati ad operazioni ammesse al tipo documento
 
+    class Meta: 
+        verbose_name = "Tipo di Documento di contabilità"
+        verbose_name_plural = "Tipi di Documento di contabilità"
+
+    def __str__(self):
+        return str(self.codDoc) + " - " + self.DesDoc
+    
+
 class PncGen (models.Model):
-    idPnc = models.BigAutoField(primary_key=True, default=0)
-    tipoDocCoge = models.ForeignKey(TipiDocCoge,on_delete=models.CASCADE,blank=True)
-    dataPnc = models.DateField('Data Nota', blank=True, default=date.today)
-    numPnc = models.CharField('Num. Nota', max_length=15,default='',blank=True)
-    statoPnc = models.CharField('Stato', max_length=30,default='',blank=True) 
-    dataCreazionePnc = models.DateField('Data di inserimento',default=date.today)
-    #pncRiga = models.ForeignKey(PncRighe,on_delete=models.CASCADE,blank=False)
-    pncRiga = models.ManyToManyField(PncRighe)
-    #timestamp e variazioni con indicazione utentee
+    tipoDoc = models.ForeignKey(TipiDocCoge,on_delete=models.CASCADE,blank=False)
+    dataCreazionePnc = models.DateField('Data di Creazione',default=date.today, editable=False)
+    numPnc = models.CharField('Numero Nota', max_length=10,default='',blank=True)
+    dataPnc = models.DateField('Data del documento',default=date.today)
+    esercizioCompetenza = models.CharField('Esercizio Contabile', max_length=4,default=str(datetime.now().year),blank=False)
+    causalePnC = models.CharField('Causale', max_length=50,default='',blank=True)
 
     class Meta: 
-        verbose_name = "Prima nota contabile"
+        verbose_name = "Prima Nota Contabile"
         verbose_name_plural = "Prime Note Contabili"
 
     def __str__(self):
-        return str(self.idPnc) + " - " + self.tipoDoc
+        return str(self.numPnc) + " - " + self.causalePnC
 
+    
+class PncRighe (models.Model):
+    idPnc = models.ForeignKey(PncGen,on_delete=models.CASCADE) #puntatore alla testata
+    idRiga = models.CharField('ID', max_length=10,default='',blank=True)
+    # tipoRiga
+    descrizioneRiga = models.CharField('Descrizione', max_length=50,default='',blank=False)
+    conto = models.ForeignKey(ContoCOGE,on_delete=models.CASCADE,blank=True, null=True) # puntatore al conto COGE
+    dare = models.DecimalField(max_digits=16, decimal_places=5,blank=True, null=True)
+    avere = models.DecimalField(max_digits=16, decimal_places=5,blank=True, null=True)
+    #timestamp e variazioni con indicazione utentee
 
-
-
-
-
+    class Meta: 
+        verbose_name = "Riga"
+        verbose_name_plural = "Righe"
+    
 class PncTes (models.Model):
-    idPnc = models.BigAutoField(primary_key=True, default=0)
-    stato = models.CharField('Stato', max_length=30,default='',blank=True)
-    datacreazione = models.DateField('Data di inserimento',default=date.today)
-    #tipoDoc = models.ForeignKey(TipiDoc,on_delete=models.CASCADE,blank=False)
-    #timestamp e variazioni con indicazione utentee  1
+    # stato = models.CharField('Stato', max_length=30,default='',blank=True)
+    # dataCreazione = models.DateField('Data di inserimento',default=date.today)
+    # tipoDoc = models.ForeignKey(TipiDoc,on_delete=models.CASCADE,blank=False)
+    # #timestamp e variazioni con indicazione utentee  
+    pass
